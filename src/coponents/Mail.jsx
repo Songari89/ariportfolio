b57379d74aa61580f619bd19b115b2c85b867977
Ari <ari89.web@gmail.com>
2024-08-01 21:33:36 +0900

    ADD FIREBASE API

--- src/coponents/Mail.jsx
+++ src/coponents/Mail.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import mailicon from "../staticimage/icons/mail.svg";
+import kakao from "../staticimage/icons/kakaotalk.png";
 import styles from "./Mail.module.css";
+import {Link} from "react-router-dom";
 
 export default function Mail() {
   return (
     <div className={styles.container}>
-      <img className={styles.icon} src={mailicon} alt="mail" />
+      <Link
+        className={styles.link}
+        to="http://pf.kakao.com/_axoxkLG"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className={styles.icon} src={kakao} alt="kakao" />
+      </Link>
     </div>
   );
 }
